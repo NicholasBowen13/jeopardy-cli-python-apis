a6e2efc455dca7a7adc1aa0eb84c82bbf83dbaeb
Nick <Nickbow1388@gmail.com>
2021-07-19 14:42:19 +0000

    Update

--- jeopardy.py
+++ jeopardy.py
@@ -1,5 +1,21 @@
 import requests
-
+import random
 response = requests.get('http://jservice.io/api/clues?category=139').json()
-
-print(response)
+score = 0
+while True:
+    user_input = input("Type P to play and Q to quit" + "\n") 
+    if user_input.upper() == "P":
+        num = random.randint(0,len(response))
+        user_response = input(response[num]["question"] + "\n" )
+        if user_response == response[num]["answer"]:
+            print("Congratulations!") 
+            score += response[num]["value"]
+        else: 
+            print("Wrong! The correct answer was " + response[num]["answer"]) 
+            score -= response[num]["value"]
+        print("Your current score is " + str(score))
+    elif user_input.upper() == "Q":
+        print("Thanks for playing! Your score was " + str(score))
+        break
+    else: 
+        print("Invalid input, please try again.")
